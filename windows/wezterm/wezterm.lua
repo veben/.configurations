7f7ed0d84998428c50b874e9c0f2ff139b188f84
veben <benoit.veyriere@gmail.com>
2024-07-25 12:11:00 +0200

    Updated Windows configurations about keyboard and Changed Wezterm configurations and removed Alacritty

--- windows/wezterm/wezterm.lua
+++ windows/wezterm/wezterm.lua
@@ -1,13 +1,22 @@
 local wezterm = require 'wezterm'
 
 local config = {}
+
 if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
--- change config now
 config.default_domain = 'WSL:Ubuntu-22.04'
 config.font = wezterm.font 'UbuntuMono Nerd Font'
 config.color_scheme = "Catppuccin Mocha"
+config.window_decorations = "NONE"
+
+config.keys = {
+  {
+      key = 'w',
+      mods = 'CTRL',
+      action = wezterm.action.CloseCurrentPane { confirm = false },
+  },
+}
 
 return config
