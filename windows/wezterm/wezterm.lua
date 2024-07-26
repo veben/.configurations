1e2f8fd8014be48fd8d224db0c7e09202e8082a0
veben <benoit.veyriere@gmail.com>
2024-07-26 23:17:50 +0200

    Improved Windows terminal and WezTerm configurations

--- windows/wezterm/wezterm.lua
+++ windows/wezterm/wezterm.lua
@@ -6,17 +6,31 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-config.default_domain = 'WSL:Ubuntu-22.04'
-config.font = wezterm.font 'UbuntuMono Nerd Font'
+------------- Theme -------------
+config.font = wezterm.font('UbuntuMono Nerd Font', { weight = 'DemiLight' })
 config.color_scheme = "Catppuccin Mocha"
-config.window_decorations = "NONE"
 
-config.keys = {
-  {
-      key = 'w',
-      mods = 'CTRL',
-      action = wezterm.action.CloseCurrentPane { confirm = false },
-  },
+local custom = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
+custom.background = "#181b1a"
+custom.tab_bar.background = "#181b1a"
+custom.tab_bar.inactive_tab.bg_color = "#181b1a"
+custom.tab_bar.new_tab.bg_color = "#181b1a"
+config.color_schemes = {
+  ["OLEDppuccin"] = custom,
 }
+config.color_scheme = "OLEDppuccin"
+
+------------- Shortcuts -------------
+config.keys = {
+  { key = 'w', mods = 'CTRL', action = wezterm.action.CloseCurrentPane { confirm = false } },
+  { key = 't', mods = 'CTRL', action = wezterm.action.SpawnTab 'CurrentPaneDomain' }
+}
+
+------------- Others -------------
+config.default_domain = 'WSL:Ubuntu-22.04'
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+config.hide_tab_bar_if_only_one_tab = true
 
 return config
